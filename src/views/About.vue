5b030a62404215e6bfcfef4553803b6549c14f71
Jesús Zúñiga Trejo <zutjmx@gmail.com>
2021-09-02 16:17:24 -0500

    Modificaciones 02/09/2021

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,81 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{titulo}}</h1>
+    <button @click="cambiarTitulo">Cambiar Titulo</button>
   </div>
 </template>
+
+<script>
+import {onBeforeMount,
+        onMounted,
+        onBeforeUpdate,
+        onUpdated,
+        onBeforeUnmount,
+        onUnmounted,
+        onErrorCaptured,
+        onRenderTracked,
+        onRenderTriggered,
+        onActivated,
+        onDeactivated,
+        ref} from 'vue'
+
+export default {
+
+  setup() {
+
+    const titulo = ref('About Page')
+
+    console.log('::Setup se llamó::')
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount')
+    })
+
+    onMounted(() => {
+      console.log('onMounted')
+    })
+
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate')
+    })
+
+    onUpdated(() => {
+      console.log('onUpdated')
+    })
+
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount')
+    })
+
+    onUnmounted(() => {
+      console.log('onUnmounted')
+    })
+
+    onErrorCaptured(() => {
+      console.log('onErrorCaptured')
+    })
+
+    onRenderTracked(() => {
+      console.log('onRenderTracked')
+    })
+
+    onRenderTriggered(() => {
+      console.log('onRenderTriggered')
+    })
+
+    onActivated(() => {
+      console.log('onActivated')
+    })
+
+    onDeactivated(() => {
+      console.log('onDeactivated')
+    })
+
+    return {
+      titulo,
+      cambiarTitulo: () => titulo.value = 'Nuevo About Page'
+    }
+
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <h1>Reactive vs Ref</h1>
+    <h3>{{arya}}</h3>
+    <h3>{{sansa}}</h3>
+    <button @click="changeArya">Cambiar Arya</button>
+    <button @click="changeSansa">Cambiar Sansa</button>
+    <h3>Arya: {{aryaIsAlive}}</h3>
+    <h3>Sansa: {{sansaIsAlive}}</h3>
+    <button @click="changeStatus">Cambiar Status</button>
   </div>
 </template>
 
 <script>
-
+import {ref, reactive} from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    
+  setup() {
+    const arya = ref({name: 'Arya Stark', age: 18})
+    const sansa = reactive({name: 'Sansa Stark', age: 20})
+
+    const aryaIsAlive = reactive(false)
+    const sansaIsAlive = ref(true)
+
+    const changeStatus = () => {
+      sansaIsAlive.value = !sansaIsAlive.value
+      aryaIsAlive = !aryaIsAlive
+    }
+
+    return {
+      arya, 
+      sansa,
+
+      aryaIsAlive,
+      sansaIsAlive,
+
+      changeStatus,
+
+      changeArya: () => {
+        arya.value.name = 'Arya Stark "La exploradora de westeros"'
+        arya.value.age = 20 
+      },
+      changeSansa: () => {
+        sansa.name = 'Sansa Stark "La reina del norte"'
+        sansa.age = 22
+      }
+    }
   }
 }
 </script>
